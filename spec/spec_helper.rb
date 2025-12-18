3c7af90004b1cb9843881a8f74fe55af94b88a5f
Fernando José <fernando.tecnicoinfo7@gmail.com>
2025-12-18 20:27:30 -0300

    refactor: ajusta a sintaxe de inclusão e filtragem no RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,10 +7,10 @@ require "rack/test"
 require_relative "../app"
 
 RSpec.configure do |config|
-  config.include Rack::Test::Methods
+  config.include(Rack::Test::Methods)
 
   config.order = :random
-  config.filter_run_when_matching :focus
+  config.filter_run_when_matching(:focus)
   config.disable_monkey_patching!
 end
 
